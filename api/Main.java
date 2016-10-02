37856d4e93919afabec02b4c8f4efa94e1395648
Joel Koreth <joelsymon@gmail.com>
2016-10-02 17:36:21 -0500

    Updated Licensing and copyright status of each source file

--- api/Main.java
+++ api/Main.java
@@ -1,4 +1,22 @@
+/*	Copyright 2016 Joel Koreth
+	This file is part of Infinite Campus API 2.0.
+
+	Infinite Campus API 2.0 is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Infinite Campus API 2.0 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU General Affero Public License
+	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import classbook.ClassbookManager;
+import classbook.Student;
 import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.Element;
--- api/classbook/Student.java
+++ api/classbook/Student.java
@@ -1,3 +1,20 @@
+/*	Copyright 2016 Joel Koreth
+	This file is part of Infinite Campus API 2.0.
+
+	Infinite Campus API 2.0 is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Infinite Campus API 2.0 is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU General Affero Public License
+	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package classbook;
 
 import calendar.Calendar;
